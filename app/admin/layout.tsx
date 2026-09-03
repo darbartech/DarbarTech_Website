c89235fce9e2c9d436a16717fb9ca197e0822e2f
Suprim-Maharjan <suprimmaharjan1921@gmail.com>
2026-09-03 17:58:43 +0545

    Added AdminNavbar and TopBar in the layout file of admin folder.

---
+++ app/admin/layout.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import RouteGuard from "@/components/common/RouteGuard";
+import { useSidebarStore } from "@/store/sidebarStore";
+import AdminNavbar from "./common/AdminNavbar";
+import Topbar from "./TopBar";
+
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { collapsed } = useSidebarStore();
+
+  return (
+    <RouteGuard allowedRoles={["superadmin", "admin"]}>
+      <div className="flex min-h-screen bg-(--bg-primary-dashboard)">
+        <AdminNavbar />
+        <main
+          id="main-content"
+          className={`min-h-screen min-w-0 flex-1 transition-all ${
+            !collapsed ? "lg:ml-64" : "lg:ml-20"
+          }`}
+        >
+          <Topbar />
+          <div className="p-4 sm:p-6">{children}</div>
+        </main>
+      </div>
+    </RouteGuard>
+  );
+}
